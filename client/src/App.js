bf233d40fd7a2f10519f555c962adefc1ac4dcec
Allie Robinson <allierob93@gmail.com>
2019-12-20 10:27:00 -0600

    Login working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import ColorList from "./components/ColorList";
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <PrivateRoute path="/colors" component={ColorList} />
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
